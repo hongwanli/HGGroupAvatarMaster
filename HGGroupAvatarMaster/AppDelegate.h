577ce4873372ddfbb01547a9f9ad2ab79b0a3e66
JoyHong <hongwanli163@gmail.com>
2015-09-16 10:21:42 +0800

    Initial Commit

---
+++ HGGroupAvatarMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HGGroupAvatarMaster
+//
+//  Created by XiaoDou on 15/9/16.
+//  Copyright (c) 2015年 北京嗨购电子商务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HGGroupAvatarMaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGGroupAvatarMaster
+//
+//  Created by XiaoDou on 15/9/16.
+//  Copyright (c) 2015年 北京嗨购电子商务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
